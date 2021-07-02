a53a7b3c884438bfde4b2af5d564258f982d1023
Shunya Niizeki <shunyaniizeki1020@gmail.com>
2021-07-02 16:40:25 -0500

    String

---
+++ 04-java-spring/StringAssignment/src/main/java/com/codingdojo/stringassignment/StringAssignmentApplication.java
@@ -0,0 +1,30 @@
+package com.codingdojo.stringassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+@RestController
+public class StringAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringAssignmentApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	
+	public String hello() {
+		return "Hello Client! How Are you doing?";
+	}
+	
+	@RequestMapping("/random")
+	
+	public String random() {
+		return "Spring Boot is great so far! So easy to just respond with strings!";
+	}
+	
+
+}
